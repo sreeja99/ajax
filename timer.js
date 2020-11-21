dcc3c6fe917e02fc5d45b49ddc7dedc057baaa72
sreeja99 <srijagodishala@gmail.com>
2020-11-21 13:26:30 +0530

    Async Nature Of JS

--- timer.js
+++ timer.js
@@ -1,1 +1,12 @@
-console.log("Welcome");
+function showTime(){
+    const date = new Date();
+    return date.getHours() +"Hrs:"+date.getMinutes() + "Mins:" +date.getSeconds()+"Secs";
+}
+
+function showSessionExpire(){
+    console.log("Activity-B: Your session expired at "+showTime());
+}
+
+console.log("Activity-A: Triggerring Activity-B at "+showTime());
+setTimeout(showSessionExpire,5000);
+console.log("Activity-A: Triggered Activity-B at "+showTime()+" will execute after 5 seconds");

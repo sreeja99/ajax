285eaa5d6fa38e92a46407ebdb34f5ce7052133c
sreeja99 <srijagodishala@gmail.com>
2020-11-22 11:05:37 +0530

    Create A Simple Html File To Test The Rest Services With Json Service Using Promise

--- http_services.js
+++ http_services.js
@@ -1,5 +1,3 @@
-let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-
 function showTime(){
     const date = new Date();
     return date.getHours() +"Hrs:"+date.getMinutes() + "Mins:" +date.getSeconds()+"Secs";
@@ -8,7 +6,7 @@ function showTime(){
 function makePromisecall(methodType,url,async=true,data=null){
     return new Promise(function(resolve,reject){
         let xhr =  new XMLHttpRequest();
-        xhr.onreadystatechange = function(){
+        xhr.onload = function(){
             if(xhr.readyState==4){
                 if(xhr.status==200||xhr.status==201){
                     resolve(xhr.responseText);
@@ -23,6 +21,14 @@ function makePromisecall(methodType,url,async=true,data=null){
                 }
             }
         }
+        
+        xhr.onerror = function(){
+            reject({
+                status: xhr.status,
+                statusText : xhr.statusText
+            });
+        };
+
         xhr.open(methodType,url,async);
         if(data){
             xhr.setRequestHeader("Content-Type","application/json");
@@ -33,25 +39,3 @@ function makePromisecall(methodType,url,async=true,data=null){
         console.log(methodType+" request sent to server at "+showTime());
     });
 }
-
-const getURL = "http://localhost:3000/employees/1";
-makePromisecall("GET",getURL,true)
-    .then(responseText => {
-        console.log("Get User Data: "+responseText)
-    })
-    .catch(error=>console.log("GET Error Status: "+JSON.stringify(error)));
-
-const deleteURL = "http://localhost:3000/employees/3";
-makePromisecall("DELETE",deleteURL,false)
-    .then(responseText => {
-        console.log("User Deleted: "+responseText)
-    })
-    .catch(error=>console.log("Delete Error Status: "+JSON.stringify(error)));
-
-const postURL = "http://localhost:3000/employees";
-const emplData = {"name":"Harry","salary":"5000"};
-makePromisecall("POST",postURL,true,emplData)
-    .then(responseText => {
-        console.log("User Added: "+responseText)
-    })
-    .catch(error=>console.log("Post Error Status: "+JSON.stringify(error)));
